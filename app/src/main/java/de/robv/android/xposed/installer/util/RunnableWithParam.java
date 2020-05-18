740866c70b1b8e532468e0fc97f18d0b779fe148
zdd <zdd2389@gmail.com>
2020-05-18 23:14:08 +0800

    xposed change to xppsed

--- app/src/main/java/de/robv/android/xposed/installer/util/RunnableWithParam.java
+++
@@ -1,5 +0,0 @@
-package de.robv.android.xposed.installer.util;
-
-public interface RunnableWithParam<T> {
-    public void run(T param);
-}
---
+++ app/src/main/java/de/robv/android/xppsed/installer/util/RunnableWithParam.java
@@ -0,0 +1,5 @@
+package de.robv.android.xppsed.installer.util;
+
+public interface RunnableWithParam<T> {
+    public void run(T param);
+}
